a47bce98e11462fd2e38cabf8beac92ec5f049a4
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-06-06 14:09:03 +0600

    feat: add advanced encoder settings

--- src/api/stream.rs
+++ src/api/stream.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use crate::services::screen::EncoderPropertyConstraint;
 use crate::state::SharedState;
 use axum::{Json, extract::State};
 use serde::{Deserialize, Serialize};
@@ -8,6 +11,7 @@ pub struct StreamSettingsDTO {
     pub bitrate: Option<u32>,
     pub resolution_percentage: Option<u8>,
     pub target_fps: Option<u64>,
+    pub encoder_properties: Option<HashMap<String, String>>,
 }
 
 #[derive(Serialize)]
@@ -18,6 +22,9 @@ pub struct CurrentSettingsResponse {
     pub max_fps: u64,
     pub native_width: i32,
     pub native_height: i32,
+    pub encoder_type: String,
+    pub encoder_properties: HashMap<String, String>,
+    pub encoder_property_constraints: HashMap<String, EncoderPropertyConstraint>,
 }
 
 pub async fn get_settings_handler(
@@ -26,6 +33,9 @@ pub async fn get_settings_handler(
     let screen = state.screen.clone();
     let s = screen.settings.lock().unwrap();
     let (native_width, native_height) = *screen.native_size.lock().unwrap();
+    let encoder_type = screen.encoder_type.lock().unwrap().clone();
+    let encoder_properties = s.encoder_properties.clone();
+    let encoder_property_constraints = screen.encoder_property_constraints.lock().unwrap().clone();
 
     Json(CurrentSettingsResponse {
         bitrate: s.bitrate,
@@ -34,6 +44,9 @@ pub async fn get_settings_handler(
         max_fps: s.max_fps,
         native_width,
         native_height,
+        encoder_type,
+        encoder_properties,
+        encoder_property_constraints,
     })
 }
 
@@ -50,6 +63,10 @@ pub async fn update_settings_handler(
 
     if let Some(fps) = payload.target_fps {
         screen.set_target_fps(fps);
+    }
+
+    if let Some(props) = payload.encoder_properties {
+        screen.set_encoder_properties(props);
     }
 
     get_settings_handler(State(state)).await
