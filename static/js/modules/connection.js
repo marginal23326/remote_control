49856636b31ff6eeac39eceae028bff96f3220dc
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-06-25 01:53:36 +0600

    fix: use built-in reconnection

--- static/js/modules/connection.js
+++ static/js/modules/connection.js
@@ -3,44 +3,30 @@ import { io } from "socket.io-client";
 import { showConnectionOverlay, hideConnectionOverlay } from "./dom.js";
 
 function initializeSocketIO(authCallback) {
-    const socket = io({ reconnection: false });
-    let reconnectionAttempts = 0;
-    const MAX_RECONNECTION_ATTEMPTS = 5;
-    let reconnectionDelay = 1000;
+    const socket = io();
 
-    const attemptReconnection = () => {
-        if (reconnectionAttempts < MAX_RECONNECTION_ATTEMPTS) {
-            reconnectionAttempts++;
-            showConnectionOverlay(`Reconnecting... Attempt ${reconnectionAttempts}`);
-            setTimeout(() => {
-                console.log("Attempting to reconnect...");
-                socket.connect();
-            }, reconnectionDelay);
-            reconnectionDelay *= 2;
-        } else {
-            showConnectionOverlay("Connection failed after multiple attempts.");
-        }
-    };
-
-    const onConnect = () => {
+    socket.on("connect", () => {
         console.log("Socket connected!");
         hideConnectionOverlay();
-        reconnectionAttempts = 0;
-        reconnectionDelay = 1000;
-    };
-
-    socket.on("connect", onConnect);
+    });
 
     socket.on("disconnect", (reason) => {
         console.log("Socket disconnected:", reason);
         if (reason !== "io client disconnect") {
-            attemptReconnection();
+            showConnectionOverlay("Disconnected. Reconnecting...");
         }
+    });
+
+    socket.io.on("reconnect_attempt", (attempt) => {
+        showConnectionOverlay(`Reconnecting... Attempt ${attempt}`);
+    });
+
+    socket.io.on("reconnect", () => {
+        hideConnectionOverlay();
     });
 
     socket.on("connect_error", (error) => {
         console.error("Socket connection error:", error);
-        attemptReconnection();
     });
 
     socket.on("auth_status", (data) => {
