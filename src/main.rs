30fcd9f8d28cdfca99c719c41c8b9abfec0bc10a
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-07-18 08:45:57 +0600

    fix: respect RUST_LOG env var

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            "remote_control=debug,tower_http=debug",
-        ))
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("remote_control=debug,tower_http=debug")),
+        )
         .with(tracing_subscriber::fmt::layer())
         .init();
 
