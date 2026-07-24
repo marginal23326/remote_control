3ec9b2d6b9603ba164a9dd78c9c9ce672d690bb0
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-07-25 02:24:44 +0600

    refactor: unify key tables

--- src/services/input/linux.rs
+++ src/services/input/linux.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use ashpd::desktop::remote_desktop::KeyState;
-use xkeysym::Keysym;
+use xkeysym::{Keysym, key};
 
-use super::keymap::shortcut_keysym;
+use super::keymap::LogicalKey;
 use crate::services::screen::linux::portal_session;
 
 #[derive(Clone)]
@@ -12,6 +12,44 @@ impl OsInputManager {
     pub fn new() -> Self {
         Self
     }
+}
+
+fn shortcut_keysym(name: &str) -> Option<u32> {
+    Some(match LogicalKey::parse(name)? {
+        LogicalKey::Shift => key::Shift_L,
+        LogicalKey::Control => key::Control_L,
+        LogicalKey::Alt => key::Alt_L,
+        LogicalKey::Super => key::Super_L,
+        LogicalKey::Return => key::Return,
+        LogicalKey::BackSpace => key::BackSpace,
+        LogicalKey::Tab => key::Tab,
+        LogicalKey::Escape => key::Escape,
+        LogicalKey::Space => key::space,
+        LogicalKey::Up => key::Up,
+        LogicalKey::Down => key::Down,
+        LogicalKey::Left => key::Left,
+        LogicalKey::Right => key::Right,
+        LogicalKey::Home => key::Home,
+        LogicalKey::End => key::End,
+        LogicalKey::PageUp => key::Page_Up,
+        LogicalKey::PageDown => key::Page_Down,
+        LogicalKey::Insert => key::Insert,
+        LogicalKey::Delete => key::Delete,
+        LogicalKey::Print => key::Print,
+        LogicalKey::F1 => key::F1,
+        LogicalKey::F2 => key::F2,
+        LogicalKey::F3 => key::F3,
+        LogicalKey::F4 => key::F4,
+        LogicalKey::F5 => key::F5,
+        LogicalKey::F6 => key::F6,
+        LogicalKey::F7 => key::F7,
+        LogicalKey::F8 => key::F8,
+        LogicalKey::F9 => key::F9,
+        LogicalKey::F10 => key::F10,
+        LogicalKey::F11 => key::F11,
+        LogicalKey::F12 => key::F12,
+        LogicalKey::Char(ch) => Keysym::from_char(ch).raw(),
+    })
 }
 
 impl super::OsInput for OsInputManager {
